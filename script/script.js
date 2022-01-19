2ad6a4720ae47f60dc86016e7e15d88d470d88e7
unknown <ijlnflhbrzdev@gmail.com>
2022-01-19 14:49:07 +0700

    +[UPDATE] New Loader

--- script/script.js
+++ script/script.js
@@ -1,10 +1,22 @@
 const loaderEvents = document.querySelector(".loader");
+const loaderAnimation = document.querySelector(".loader-animation");
 console.log(loaderEvents)
 window.addEventListener('DOMContentLoaded', () => {
-     loaderEvents.classList.remove("d-none");
-     setTimeout(() => {
+     if (loaderAnimation) {
+          loaderAnimation.classList.remove("d-none");
+     }
 
-          loaderEvents.classList.add("d-none");
+     if (loaderEvents) {
+          loaderEvents.classList.remove("d-none");
+     }
+     setTimeout(() => {
+          if (loaderAnimation) {
+               loaderAnimation.classList.add("d-none");
+          }
+          if (loaderEvents) {
+
+               loaderEvents.classList.add("d-none");
+          }
      }, 1000);
 
 });
@@ -13,3 +25,4 @@ window.addEventListener('DOMContentLoaded', () => {
 $(".navbar-toggle").click(function () {
      $(".open").toggle(500).siblings().toggle(500);
 });
+console.log("global")
